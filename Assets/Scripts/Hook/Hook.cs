6fb91ee2603f12df784047a9e539114f7750ed58
Dez <68178086+Dezato108@users.noreply.github.com>
2022-10-27 14:31:10 +0700

    Y movement of the hook
    WTF is this code ?

--- Assets/Scripts/Hook/Hook.cs
+++ Assets/Scripts/Hook/Hook.cs
@@ -15,7 +15,7 @@ public class Hook : MonoBehaviour
 
     private Collider2D coll;
 
-    private bool canMove = true;
+    private bool canMove;
 
     //List of fishes
 
@@ -43,5 +43,57 @@ public class Hook : MonoBehaviour
             position.x = vector.x;
             transform.position = position;
         }
+    }
+
+    public void StartFishing()
+    {
+        length = -50; //IdleManager
+        strength = 3; //IdleManager
+        fishCount = 0;
+        float time = (-length) * .1f;
+
+        cameraTween = mainCamera.transform.DOMoveY(length, 1 + time * .25f, false).OnUpdate(delegate
+          {
+              if (mainCamera.transform.position.y <= -11)
+              {
+                  transform.SetParent(mainCamera.transform);
+              }
+          }).OnComplete(delegate{
+            coll.enabled = true;
+            cameraTween = mainCamera.transform.DOMoveY(0, time*5, false).OnUpdate(delegate 
+            {
+                if (mainCamera.transform.position.y <= -25f)
+                {
+                    StopFishing();
+                }
+            });
+          });
+
+        //Screen(GAME)
+        coll.enabled = false;
+        canMove = true;
+        //Clear the hook
+    }
+
+    void StopFishing()
+    {
+        canMove = false;
+        cameraTween.Kill(false);
+        cameraTween = mainCamera.transform.DOMoveY(0, 2, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y >= -11)
+            {
+                transform.SetParent(null);
+                transform.position = new Vector2(transform.position.x, -6);
+            }
+        }).OnComplete(delegate
+        {
+            transform.position = Vector2.down * 6;
+            coll.enabled = true;
+            int num = 0;
+            //Clear out the hook from the fishes
+            //IdleManager Totalgain = num
+            //SceneManager End Screen
+        });
     }
 }
