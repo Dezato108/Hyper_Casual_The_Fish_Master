5fddc1fe238f1e4141a94349506b88d47630083e
Dez <hiepg2@gmail.com>
2022-10-31 20:32:30 +0700

    Adding offline earnings
    Player can get money when not playing the game

---
+++ Assets/Scripts/Managers Scripts/IdleManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class IdleManager : MonoBehaviour
+{
+    [HideInInspector]
+    public int length;
+
+    [HideInInspector]
+    public int strength;
+
+    [HideInInspector]
+    public int offlineEarnings;
+
+    [HideInInspector]
+    public int lengthCost;
+
+    [HideInInspector]
+    public int strengthCost;
+
+    [HideInInspector]
+    public int offlineEarningsCost;
+
+    [HideInInspector]
+    public int wallet;
+
+    [HideInInspector]
+    public int totalGain;
+
+    private int[] costs = new int[]
+    {
+        120, 151, 197, 250, 324, 414, 537, 687, 892, 1000, 1444, 1911, 2479, 3128, 4192, 5421, 7584, 9000, 11543
+    };
+
+    public static IdleManager instance;
+
+    private void Awake()
+    {
+        if (IdleManager.instance)
+        {
+            UnityEngine.Object.Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        length = -PlayerPrefs.GetInt("Length", 30);
+        strength = PlayerPrefs.GetInt("Strength", 3);
+        offlineEarnings = PlayerPrefs.GetInt("Offline", 3);
+        lengthCost = costs[-length / 10 - 3];
+        strengthCost = costs[strength - 3];
+        offlineEarningsCost = costs[offlineEarnings - 3];
+        wallet = PlayerPrefs.GetInt("Wallet", 0);
+    }
+
+    private void OnApplicationPause(bool paused)
+    {
+        if (paused)
+        {
+            DateTime now = DateTime.Now;
+            PlayerPrefs.SetString("Date", string.Empty);
+            MonoBehaviour.print(now.ToString());
+        }
+        else
+        {
+            string @string = PlayerPrefs.GetString("Date", string.Empty);
+            if (@string != string.Empty)
+            {
+                DateTime d = DateTime.Parse(@string);
+                totalGain = (int)((DateTime.Now - d).TotalMinutes * offlineEarnings + 1.0);
+                //screen manager return
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        OnApplicationPause(true);
+    }
+
+    public void BuyLength()
+    {
+        length -= 10;
+        wallet -= lengthCost;
+        lengthCost = costs[-length / 10 - 3];
+        PlayerPrefs.SetInt("Length", -length);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //screen manager main
+    }
+
+    public void BuyStrength()
+    {
+        strength += 1;
+        wallet -= strengthCost;
+        strengthCost = costs[strength - 3];
+        PlayerPrefs.SetInt("Strength", strength);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //screen manager main
+    }
+
+    public void BuyOfflineEarnings()
+    {
+        offlineEarnings += 1;
+        wallet -= offlineEarningsCost;
+        offlineEarningsCost = costs[offlineEarnings - 3];
+        PlayerPrefs.SetInt("Offline", offlineEarnings);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //screen manager main
+    }
+
+    public void CollectMoney()
+    {
+        wallet += totalGain;
+        PlayerPrefs.SetInt("Wallet", wallet);
+    }
+
+    public void CollectDoubleMoney()
+    {
+        wallet += totalGain * 2;
+        PlayerPrefs.SetInt("Wallet", wallet);
+    }
+}
